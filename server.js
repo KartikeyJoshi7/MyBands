297ebf5345d7171370330b01b2889237a0f541ad
Kartikey Joshi <joshikartikey298@gmail.com>
2019-10-10 11:10:59 +0530

    signin and signout functionality

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+var session = require('express-session');
+
+const app = express();
+
+app.use(session({ secret: 'Kartikey', saveUninitialized: true, resave: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.use('/', require('./routes').route)
+
+app.set('port', process.env.PORT || 7000)
+app.listen(app.get('port'), function () {
+    console.log('server started at http://localhost:7000/');
+
+}) 
+ 
