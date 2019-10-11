ec8684377e9680ca1d5bd829bdd187c94824da0d
Kartikey Joshi <joshikartikey298@gmail.com>
2019-10-11 16:46:57 +0530

    Added CSS to views.

--- routes/users.js
+++ routes/users.js
@@ -1,98 +1,106 @@
-const User = require('../models').User
-const route = require('express').Router()
-var bcrypt = require('bcryptjs');
-var jwt = require('jsonwebtoken');
+const User = require('../models').User
+const route = require('express').Router()
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
-route.post('/signin', (req, res) => {
-    User.findOne({
-        where: {
-            email: req.body.email
-        }
-    })
-        .then((users) => {
-            if (users) {
-                if (!bcrypt.compareSync(req.body.password, users.password)) {
-                    req.session.user = null;
-                    return res.status(401).send({
-                        title: 'Login failed',
-                        error: { message: 'Invalid login credentials' }
-                    });
-                }
-                console.log(users["dataValues"].id);
-                req.session.user = users;
-                // var token = jwt.sign({ user: users }, 'secret', { expiresIn: 720000 });
-                res.status(200).send({
-                    message: 'Successfully logged in',
-                    //token: token,
-                    userId: users["dataValues"].id
-                });
-            }
+route.post('/signin', (req, res) => {
+    console.log(req.body);
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+        .then((users) => {
+            if (users) {
+                if (!bcrypt.compareSync(req.body.password, users.password)) {
+                    req.session.user = null;
+                    return res.status(401).send({
+                        title: 'Login failed',
+                        error: { message: 'Invalid login credentials' }
+                    });
+                }
+                console.log(users["dataValues"].id);
+                req.session.user = users;
+                // var token = jwt.sign({ user: users }, 'secret', { expiresIn: 720000 });
+                res.redirect('/bands/');
+            }
 
-            else
-            {
-                req.session.user = null;
-                res.status(500).send({
-                    error: "Invalid email or password"
-                })
-            }
-        })
-        .catch((err) => {
-            req.session.user = null;
-            res.status(500).send({
-                error: "Invalid email or password"
-            })
-        })
+            else {
+                req.session.user = null;
+                res.status(500).send({
+                    error: "Invalid email or password"
+                })
+            }
+        })
+        .catch((err) => {
+            req.session.user = null;
+            res.status(500).send({
+                error: "Invalid email or password"
+            })
+        })
 
 })
 
-route.post('/', (req, res) => {
-    // We expect the req to have name,email,password,address,college,phn number in it
-    // We will create a new user 
-    console.log("in post");
-    console.log(req.body.password);
-    console.log(req.body);
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        college: req.body.college,
-        dob: req.body.dob,
-        password: bcrypt.hashSync(req.body.password, 10)
-    }).then((user) => {
-        res.status(201).send(user)
-    }).catch((err) => {
-        console.log(err);
+route.get('/signin', (req, res) => {
+    if (req.session.user) {
+        res.render('home');
+    } else {
+        res.render("login");
+    }
+});
 
-        res.status(501).send({
+route.get('/signup',(req, res) => {
+    res.render('Signup');
+});
 
-            error: err.errors
-        })
-    })
+route.post('/signup', (req, res) => {
+    // We will create a new user 
+    console.log("in post");
+    console.log(req.body.password);
+    console.log(req.body);
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        college: req.body.college,
+        dob: req.body.dob,
+        password: bcrypt.hashSync(req.body.password, 10)
+    }).then((user) => {
+        res.status(201).send(user)
+    }).catch((err) => {
+        console.log(err);
+
+        res.status(501).send({
+
+            error: err.errors
+        })
+    })
+    res.redirect('/users/signin');
 })
 
-route.get('/logout', function (req, res) {
-    req.session.destroy(function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.status(200).send({
-                msg: "successfully logged out"
-            })
-        }
-    });
+route.get('/logout', function (req, res) {
+    console.log("xfvfg");
+    req.session.destroy(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.status(200).send({
+                msg: "successfully logged out"
+            })
+        }
+    });
 })
 
-route.get('/isloggedin', function (req, res) {
-    if(req.session.user)
-    {
-        res.send({
-            done:true,
-            name:req.session.user.name
-        })
-    }
-    else
-    res.send({
-        done:false,
-    })
+route.get('/isloggedin', function (req, res) {
+    if (req.session.user) {
+        res.send({
+            done: true,
+            name: req.session.user.name
+        })
+    }
+    else
+        res.send({
+            done: false,
+        })
 })
 
-exports = module.exports = route 
+exports = module.exports = route 
